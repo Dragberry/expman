2589b3c0b64030bdeb1623ad2b85829000d5a529
MaksimDragun <MaksimDragun@live.com>
2015-10-31 02:07:27 +0300

    DEV:TASK-1:Delete duplicate config class

--- expman-web/src/main/java/net/dragberry/expman/application/config/SecurityInitializer.java
+++
@@ -1,5 +0,0 @@
-package net.dragberry.expman.application.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer { }
